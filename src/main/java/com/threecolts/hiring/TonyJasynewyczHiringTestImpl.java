b15f685ac452f84b56c9f7947263f4bc07ad79ed
Tony Jasynewycz <tony_jas@hotmail.com>
2023-11-16 17:49:31 +0000

    Emailed Solution (too late)

--- src/main/java/com/threecolts/hiring/TonyJasynewyczHiringTestImpl.java
+++ src/main/java/com/threecolts/hiring/TonyJasynewyczHiringTestImpl.java
@@ -1,21 +1,20 @@
 package com.threecolts.hiring;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
+
+/**
+ * Class to be held in a set to represent unique URL's via "equals/hashCode"
+ */
 public class TonyJasynewyczHiringTestImpl implements HiringTest {
 
     private class MyUrl {
-        String protocol;
         String domain;
-        String subdomain;
-        List <String> parts;
+        List <String> params;
 
-        public MyUrl(String protocol, String domain, String subdomain, List<String> parts) {
-            this.protocol = protocol;
+        public MyUrl(String domain, List<String> params) {
             this.domain = domain;
-            this.subdomain = subdomain;
-            this.parts = parts;
+            this.params = params;
         }
 
         @Override
@@ -23,38 +22,46 @@ public class TonyJasynewyczHiringTestImpl implements HiringTest {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             MyUrl myUrl = (MyUrl) o;
-            return Objects.equals(protocol, myUrl.protocol) && Objects.equals(domain, myUrl.domain) && Objects.equals(subdomain, myUrl.subdomain) && Objects.equals(parts, myUrl.parts);
+            return Objects.equals(domain, myUrl.domain) && Objects.equals(params, myUrl.params);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(protocol, domain, subdomain, parts);
+            return Objects.hash(domain, params);
         }
-
     }
 
     @Override
     public int countUniqueUrls(List<String> urls) {
 
+
         Set<MyUrl> uniqueUrls = new HashSet<>();
 
         for(String url : urls) {
-            String[] parts = url.split("/");
 
-            System.out.println("Start");
-            for(String part : parts) {
-                System.out.println(part);
+            String[] paramParts = null;
+
+            String paramsTemp = null;
+            String urlOnly = null;
+
+            if(url.contains("?")) {
+                String[] parts = url.split("[?]");
+                urlOnly = parts[0];
+                if(parts.length > 1) {
+                    paramsTemp = parts[1];
+                    paramParts = paramsTemp.split("&");
+                    Arrays.sort(paramParts);
+                }
+            } else {
+                urlOnly = url;
             }
-            StringBuilder subDomain = new StringBuilder();
 
-            for(int i = 3; i < parts.length; i++) {
-                subDomain.append(parts[i]);
+            // if we have a trailing / then remove it
+            if(urlOnly.endsWith("/")) {
+                urlOnly = urlOnly.substring(0, urlOnly.length() - 1);
             }
-            System.out.println(subDomain.toString());
-            String[] parts2 = subDomain.toString().split("\\?");
 
-
-            MyUrl temp = new MyUrl(parts[0], parts[2], "", Collections.EMPTY_LIST);
+            MyUrl temp = new MyUrl(urlOnly, (paramParts != null && paramParts.length > 0) ? Arrays.asList(paramParts) : null);
             uniqueUrls.add(temp);
         }
 
@@ -63,16 +70,28 @@ public class TonyJasynewyczHiringTestImpl implements HiringTest {
 
     @Override
     public Map<String, Integer> countUniqueUrlsPerTopLevelDomain(List<String> urls) {
-        List<MyUrl> domains = new ArrayList<>();
+
+        Map<String, Integer> map = new HashMap<>();
 
         for(String url : urls) {
-            String[] parts = url.split("/");
 
-            MyUrl temp = new MyUrl(parts[0], parts[1], "", Collections.EMPTY_LIST);
-            domains.add(temp);
+            String domain = null;
+
+            // get domain
+            String trimmed = url.substring(0, url.indexOf(".com"));
+            trimmed = trimmed.substring(trimmed.indexOf("//")+2);
+            if(trimmed.contains(".")) {
+
+                String[] temp = trimmed.split("[.]");
+                domain = temp[temp.length-1] + ".com";
+            } else {
+                domain = trimmed + ".com";
+            }
+
+            if(map.computeIfPresent(domain, (k, v) -> Integer.valueOf(v+1)) == null) {
+                map.put(domain, 1);
+            }
         }
-
-        //return domains.stream().distinct().collect(Collectors.toMap());
-        return null;
+        return map;
     }
 }
--- src/test/java/com/threecolts/hiring/TestHiring.java
+++ src/test/java/com/threecolts/hiring/TestHiring.java
@@ -3,28 +3,51 @@ package com.threecolts.hiring;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestHiring {
 
     @Test
-    public void testHiring() {
+    public void test1Hiring() {
 
         String test1 = "https://example.com";
         String test2 = "https://example.com/";
 
         List<String> list1 = List.of(test1, test2);
 
+        TonyJasynewyczHiringTestImpl tj = new TonyJasynewyczHiringTestImpl();
+        assertEquals(1, tj.countUniqueUrls(list1));
+
+    }
+
+    @Test
+    public void test2Hiring() {
+
         String test3 = "https://example.com";
         String test4 = "http://example.com";
 
         List<String> list2 = List.of(test3, test4);
 
+        TonyJasynewyczHiringTestImpl tj = new TonyJasynewyczHiringTestImpl();
+        assertEquals(2, tj.countUniqueUrls(list2));
+    }
+
+    @Test
+    public void test3Hiring() {
+
         String test5 = "https://example.com?";
         String test6 = "https://example.com";
 
         List<String> list3 = List.of(test5, test6);
+
+        TonyJasynewyczHiringTestImpl tj = new TonyJasynewyczHiringTestImpl();
+        assertEquals(1, tj.countUniqueUrls(list3));
+    }
+
+    @Test
+    public void test4Hiring() {
 
         String test7 = "https://example.com?a=1&b=2";
         String test8 = "https://example.com?b=2&a=1";
@@ -32,10 +55,21 @@ public class TestHiring {
         List<String> list4 = List.of(test7, test8);
 
         TonyJasynewyczHiringTestImpl tj = new TonyJasynewyczHiringTestImpl();
-        assertEquals(1, tj.countUniqueUrls(list1));
-        assertEquals(2, tj.countUniqueUrls(list2));
-        assertEquals(1, tj.countUniqueUrls(list3));
         assertEquals(1, tj.countUniqueUrls(list4));
+    }
 
+    @Test
+    public void testDomain1() {
+
+        String test1 = "https://example.com";
+        String test2 = "https://subdomain.example.com";
+        String test3 = "https://subdomain.example2.com";
+
+        List<String> list = List.of(test1, test2, test3);
+
+        TonyJasynewyczHiringTestImpl tj = new TonyJasynewyczHiringTestImpl();
+
+        assertEquals(2, tj.countUniqueUrlsPerTopLevelDomain(list).get("example.com").intValue());
+        assertEquals(1, tj.countUniqueUrlsPerTopLevelDomain(list).get("example2.com").intValue());
     }
 }
